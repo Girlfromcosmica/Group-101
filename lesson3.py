925d91e2e5fb36b251f3cb8e9e0b0ab96c9793bd
vika shioshvili <113729991+Girlfromcosmica@users.noreply.github.com>
2022-10-13 15:18:56 +0400

    Add files via upload

---
+++ lesson3.py
@@ -0,0 +1,29 @@
+# num1 = input("enter number1: ")
+# num2 = input("enter number2: ")
+# print("their sum is" + str(int(num1)+int(num2)))
+
+from re import X
+from turtle import xcor
+
+
+# num1 = input("enter number1: ")
+# num2 = input("enter number2: ")
+# print("their sum is" + str(int(num1) + int(num2)))
+
+
+
+num3 = int(input("enter number3:"))
+num4 = int(input("enter number4:"))
+print("their sum is" + str((num3) * (num4)))
+product=num3*num4
+if product > 50 :
+    print("xxx")
+else:
+    print("yyy")
+
+
+    
+
+
+    
+
