55ed94efd121392786a3ed6eb308e87f2c8d366a
vika shioshvili <113729991+Girlfromcosmica@users.noreply.github.com>
2022-10-13 15:22:00 +0400

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,118 @@
+#gadavwyvite ramodenime metodit mecada
+
+#______________________________________________________________________________
+#input methodit
+# my_list=["xinkali","mwvadi","lobiani","kababi","xachapuri","wather"]
+# prices=[2,20,15,10,15,2]
+# print(my_list)
+# print(prices)
+# the_price_of_xinkali=input("how much is xinkali?: ")
+# print("xinkali costs" " " + the_price_of_xinkali + " " "gel")
+# the_price_of_mwvadi=input("how much is mwvadi?:  ")
+# print("mwvadi costs" " " + the_price_of_mwvadi + " " "gel")
+# the_price_of_lobiani=input("how much is lobiani?: ")
+# print("lobiani costs" " " + the_price_of_lobiani + " " "gel")
+# the_price_of_kababi=input("how much is kababi?: ")
+# print("lobiani costs" " " + the_price_of_kababi +" " "gel")
+# the_price_of_xachapuri=input("how much is xachapuri?: ")
+# print("xachapuri costs" " " + the_price_of_xachapuri + " " "gel")
+# the_price_of_wather=input("how much is wather?: ")
+# print("wather costs" " " + the_price_of_wather + " " "gel")
+
+
+#______________________________________________________________________________
+
+
+#ak gamoviyene grdzel stringad gardakmnit martivad winadadebis daprintvis metodi
+# my_sentence='''axali pasebit xinkali girs 2 lari ,
+# mwvadi girs 20 lari , 
+# lobiani girs 15 lari,
+# kababi girs 10 lari ,
+# xachapuri girs15 lari 
+# da wyali girs 2 lari'''
+# print(my_sentence)
+
+
+#______________________________________________________________________________
+
+
+#indeksireba [] mivwvdet raime indeksze mdgoms
+# my_list=['''xinkali girs 2 lari",
+# "mwvadi girs 20 lari",
+# "lobiani girs 15 lari",
+# "kababi girs 10 lari",
+# "xachapuri girs 15 lari",
+# "wyali girs 2 lari''']
+# print(my_list[0])
+# print(my_list[1])
+# print(my_list[2])
+# print(my_list[3])
+# print(my_list[4])
+# print(my_list[5])
+
+
+#______________________________________________________________________________
+
+
+#"xinkali","mwvadi","lobiani","kababi","xachapuri","wather"
+#2,20,15,10,15,2
+#format metodit 
+# xinkali=2
+# mwvadi=20
+# lobiani=15
+# kababi=10
+# xachapuri=15
+# wyali=2
+# print('''xinkali girs {} lari,
+# mwvadi girs {} lari,
+# lobiani girs {} lari,
+# kababi girs {} lari,
+# xachapuri girs {} lari,
+# wyali girs {} lari'''.format(xinkali,mwvadi,lobiani,kababi,xachapuri,wyali))
+
+
+#_____________________________________________________________________________
+
+
+xinkali_price=2
+mwvadi_price=20
+lobiani_price=15
+kababi_price=10
+xachapuri_price=15
+wather_price=2
+
+print("xinkali girs" " " + str(xinkali_price) + " " "lari")
+print("mwvadi girs" " " + str(mwvadi_price) + " " "lari")
+print("lobiani girs" " " + str(lobiani_price) + " " "lari")
+print("kababi girs" " " + str(kababi_price) + " " "lari")
+print("xachapuri girs" " " + str(xachapuri_price) + " " "lari")
+print("wyali girs" " "+ str(wather_price) + " " "lari")
+
+
+#____________________________________________________________________________
+
+
+#erorrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr
+#stringistvis metodis gamodzaxebis metodit
+# menu1="xinkali girs ? lari"
+# menu2="mwvadi girs ?? lari"
+# menu3="lobiani girs ?? lari"
+# menu4="kababi girs ?? lari"
+# menu5="achapuri girs ?? lari"
+# menu6="wyali girs ? lari"
+
+# print(menu1.replace("?" , "2"))
+# print(menu2.replace("??" "20"))
+# print(menu3.replace("??" , "15"))
+# print(menu4.replace("??" ,"10"))
+# print(menu5.replace("??" ,"15"))
+# print(menu6.replace("?" ,"2"))
+
+
+
+
+
+
+
+
+
